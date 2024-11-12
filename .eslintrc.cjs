40a127c01ebce9872ab0e2450aaf51f4ee4d2946
Jannik Zinkl <jannik@trieb.work>
2024-11-12 22:37:15 +0100

    fix: disable rule next image for now

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,10 @@ module.exports = {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
+  rules: {
+    /**
+     * Just ignore this rule for now during the migration of shadcnblocks
+     */
+    '@next/next/no-img-element': 'off',
+  },
 }
