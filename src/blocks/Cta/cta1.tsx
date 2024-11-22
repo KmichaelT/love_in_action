3ea279f561c23a187f1ef729c9884ff35acf83c9
Jannik Zinkl <jannik@trieb.work>
2024-11-22 13:14:59 +0100

    feat: CTA1

--- src/blocks/Cta/cta1.tsx
+++ src/blocks/Cta/cta1.tsx
@@ -1,9 +1,19 @@
-import { ArrowRight, Sparkles } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { CtaBlock } from '@/payload-types';
+import { Icon } from '@/components/Icon';
+import RichText from '@/components/RichText';
+import { splitRichText } from '@/utilities/richtext';
+import { Media } from '@/components/Media';
 
-const Cta1 = () => {
+const Cta1: React.FC<CtaBlock> = ({ icon, richText, image }) => {
+  const { firstNode, rest } = splitRichText(richText, {
+    splitOn: ['h2', 'h3', 'h4'],
+    takeFirst: true
+  });
+
   return (
     <section className="py-32">
       <div className="container max-w-5xl">
@@ -11,23 +21,31 @@ const Cta1 = () => {
           <div className="p-6 md:max-w-96">
             <div className="mb-2 flex items-center gap-2">
               <span className="flex size-7 items-center justify-center rounded-full bg-muted">
-                <Sparkles className="size-4" strokeWidth={1.5} />
+                {icon && <Icon icon={icon} className="size-4" strokeWidth={1.5} /> }
               </span>
-              <h4 className="text-2xl font-bold">Integrations</h4>
+              {firstNode && (
+                <div className="[&_h1]:text-2xl [&_h1]:font-bold [&_h2]:text-2xl [&_h2]:font-bold [&_h3]:text-2xl [&_h3]:font-bold [&_h4]:text-2xl [&_h4]:font-bold">
+                  <RichText content={firstNode} withWrapper={false} />
+                </div>
+              )}
             </div>
-            <p className="text-muted-foreground">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Architecto illo praesentium nisi, accusantium quae.
-            </p>
+            {rest && (
+              <div className="[&_p]:text-muted-foreground">
+                <RichText content={rest} withWrapper={false} />
+              </div>
+            )}
             <Button className="mt-8">
               Get Started <ArrowRight className="ml-2 size-4" />
             </Button>
           </div>
-          <img
-            src="https://www.shadcnblocks.com/images/block/placeholder-1.svg"
-            alt="placeholder"
-            className="aspect-video object-cover md:max-w-96"
-          />
+          {image && (
+            <Media 
+              resource={image}
+              htmlElement={null}
+              className="aspect-video object-cover md:max-w-96"
+              imgClassName="aspect-video object-cover md:max-w-96"
+            />
+          )}
         </Card>
       </div>
     </section>
