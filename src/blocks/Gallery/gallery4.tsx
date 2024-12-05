64be59c64da22633735f53a8959ffa307bff7453
Jannik Zinkl <jannik@trieb.work>
2024-12-05 15:17:00 +0100

    feat: gallery4 done

--- src/blocks/Gallery/gallery4.tsx
+++ src/blocks/Gallery/gallery4.tsx
@@ -1,95 +1,45 @@
-'use client';
+'use client'
 
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { ArrowLeft, ArrowRight } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 
-import { Button } from '@/components/ui/button';
-import type { CarouselApi } from '@/components/ui/carousel';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from '@/components/ui/carousel';
+import { Button } from '@/components/ui/button'
+import type { CarouselApi } from '@/components/ui/carousel'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import { GalleryBlock } from '@/payload-types'
+import { Media } from '@/components/Media'
+import RichText from '@/components/RichText'
 
-const data = [
-  {
-    id: 'item-1',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
-    image:
-      'https://images.unsplash.com/photo-1551250928-243dc937c49d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxMjN8fHx8fHwyfHwxNzIzODA2OTM5fA&ixlib=rb-4.0.3&q=80&w=1080',
-  },
-  {
-    id: 'item-2',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
-    image:
-      'https://images.unsplash.com/photo-1551250928-e4a05afaed1e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxMjR8fHx8fHwyfHwxNzIzODA2OTM5fA&ixlib=rb-4.0.3&q=80&w=1080',
-  },
-  {
-    id: 'item-3',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
-    image:
-      'https://images.unsplash.com/photo-1536735561749-fc87494598cb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxNzd8fHx8fHwyfHwxNzIzNjM0NDc0fA&ixlib=rb-4.0.3&q=80&w=1080',
-  },
-  {
-    id: 'item-4',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
-    image:
-      'https://images.unsplash.com/photo-1548324215-9133768e4094?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxMzF8fHx8fHwyfHwxNzIzNDM1MzA1fA&ixlib=rb-4.0.3&q=80&w=1080',
-  },
-  {
-    id: 'item-5',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
-    image:
-      'https://images.unsplash.com/photo-1550070881-a5d71eda5800?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxMjV8fHx8fHwyfHwxNzIzNDM1Mjk4fA&ixlib=rb-4.0.3&q=80&w=1080',
-  },
-];
 
-const Gallery4 = () => {
-  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
-  const [canScrollPrev, setCanScrollPrev] = useState(false);
-  const [canScrollNext, setCanScrollNext] = useState(false);
+const Gallery4: React.FC<GalleryBlock> = ({ elements }) => {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi>()
+  const [canScrollPrev, setCanScrollPrev] = useState(false)
+  const [canScrollNext, setCanScrollNext] = useState(false)
   useEffect(() => {
     if (!carouselApi) {
-      return;
+      return
     }
     const updateSelection = () => {
-      setCanScrollPrev(carouselApi.canScrollPrev());
-      setCanScrollNext(carouselApi.canScrollNext());
-    };
-    updateSelection();
-    carouselApi.on('select', updateSelection);
+      setCanScrollPrev(carouselApi.canScrollPrev())
+      setCanScrollNext(carouselApi.canScrollNext())
+    }
+    updateSelection()
+    carouselApi.on('select', updateSelection)
     return () => {
-      carouselApi.off('select', updateSelection);
-    };
-  }, [carouselApi]);
+      carouselApi.off('select', updateSelection)
+    }
+  }, [carouselApi])
   return (
     <section className="py-32">
       <div className="container">
         <div className="mb-8 flex items-end justify-between md:mb-14 lg:mb-16">
-          <h2 className="text-3xl font-medium md:text-4xl lg:text-5xl">
-            All case studies
-          </h2>
+          <h2 className="text-3xl font-medium md:text-4xl lg:text-5xl">All case studies</h2>
           <div className="hidden shrink-0 gap-2 md:flex">
             <Button
               size="icon"
               variant="ghost"
               onClick={() => {
-                carouselApi?.scrollPrev();
+                carouselApi?.scrollPrev()
               }}
               disabled={!canScrollPrev}
               className="disabled:pointer-events-auto"
@@ -100,7 +50,7 @@ const Gallery4 = () => {
               size="icon"
               variant="ghost"
               onClick={() => {
-                carouselApi?.scrollNext();
+                carouselApi?.scrollNext()
               }}
               disabled={!canScrollNext}
               className="disabled:pointer-events-auto"
@@ -122,40 +72,46 @@ const Gallery4 = () => {
           }}
         >
           <CarouselContent className="ml-[calc(theme(container.padding)-20px)] mr-[calc(theme(container.padding))] 2xl:ml-[calc(50vw-700px+theme(container.padding)-20px)] 2xl:mr-[calc(50vw-700px+theme(container.padding))]">
-            {data.map((item) => (
-              <CarouselItem
-                key={item.id}
-                className="max-w-[320px] pl-[20px] lg:max-w-[360px]"
-              >
-                <a href={item.href} className="group rounded-xl">
-                  <div className="group relative h-full min-h-[27rem] max-w-full overflow-hidden rounded-xl bg-red-200 md:aspect-[5/4] lg:aspect-[16/9]">
-                    <img
-                      src={item.image}
-                      alt={item.title}
-                      className="absolute size-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
-                    />
-                    <div className="absolute inset-0 h-full bg-[linear-gradient(hsl(var(--primary)/0.2),hsl(var(--primary)/0.8)_100%)] mix-blend-multiply" />
-                    <div className="absolute inset-x-0 bottom-0 flex flex-col items-start p-6 text-primary-foreground md:p-8">
-                      <div className="mb-2 pt-4 text-xl font-semibold md:mb-3 md:pt-4 lg:pt-4">
-                        {item.title}
-                      </div>
-                      <div className="mb-8 line-clamp-2 md:mb-12 lg:mb-9">
-                        {item.description}
-                      </div>
-                      <div className="flex items-center text-sm">
-                        Read more{' '}
-                        <ArrowRight className="ml-2 size-5 transition-transform group-hover:translate-x-1" />
+            {elements &&
+              elements.map((item) => (
+                <CarouselItem key={item.id} className="max-w-[320px] pl-[20px] lg:max-w-[360px]">
+                  <a href={item.link?.url || '#'} className="group rounded-xl">
+                    <div className="group relative h-full min-h-[27rem] max-w-full overflow-hidden rounded-xl bg-red-200 md:aspect-[5/4] lg:aspect-[16/9]">
+                      {item.image && (
+                        <Media
+                          resource={item.image}
+                          className="absolute size-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                          imgClassName="absolute size-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                          fill
+                        />
+                      )}
+                      <div className="absolute inset-0 h-full bg-[linear-gradient(hsl(var(--primary)/0.2),hsl(var(--primary)/0.8)_100%)] mix-blend-multiply" />
+                      <div className="absolute inset-x-0 bottom-0 flex flex-col items-start p-6 text-primary-foreground md:p-8">
+                        {item.richText && (
+                          <RichText
+                            content={item.richText}
+                            overrideStyle={{
+                              h3: 'mb-2 pt-4 text-xl font-semibold md:mb-3 md:pt-4 lg:pt-4',
+                              h4: 'mb-2 pt-4 text-xl font-semibold md:mb-3 md:pt-4 lg:pt-4',
+                              p: 'mb-8 line-clamp-2 md:mb-12 lg:mb-9',
+                            }}
+                          />
+                        )}
+                        {/* We can't use CMSLinks here, because we would build <a> as descendant of <a> */}
+                        <div className="flex items-center text-sm">
+                          {item.link?.label || 'Read more'}
+                          <ArrowRight className="ml-2 size-5 transition-transform group-hover:translate-x-1" />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
-              </CarouselItem>
-            ))}
+                  </a>
+                </CarouselItem>
+              ))}
           </CarouselContent>
         </Carousel>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Gallery4;
+export default Gallery4
