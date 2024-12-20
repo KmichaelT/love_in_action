eeb3b9f09e7d929ce8707d7cd138594e79caca80
Jannik Zinkl <jannik@trieb.work>
2024-12-20 14:31:52 +0100

    fix: just boilerplate

--- src/heros/CustomHero/index.tsx
+++ src/heros/CustomHero/index.tsx
@@ -1,8 +1,8 @@
-import Hero17 from './hero17';
+// import Hero17 from './hero17';
 
 // Add new custom heroes here
 const customHeros: { [key: string]: React.FC<any> } = {
-  17: Hero17,
+  // 17: Hero17,
   // Add more custom heroes like: 18: Hero18,
 };
 
