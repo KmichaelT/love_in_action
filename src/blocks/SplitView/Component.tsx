f07251b3de67b688b0a7849a89056b6386bc6b5e
Jannik Zinkl <jannik@trieb.work>
2025-01-03 20:05:54 +0100

    fix: splitview positioning fixes

--- src/blocks/SplitView/Component.tsx
+++ src/blocks/SplitView/Component.tsx
@@ -17,7 +17,7 @@ const colsSpanClasses = {
 const bgColorClasses = {
   background: 'bg-background',
   accent: 'bg-accent',
-  primary: 'bg-primary'
+  primary: 'bg-primary',
 }
 
 /**
@@ -29,23 +29,17 @@ export const SplitViewBlock: React.FC<Props> = (props) => {
   if (!columns?.length) return null
 
   return (
-    <section className={cn('w-full py-16', bgColorClasses[backgroundColor])}>
-      <div className="container">
-        <div className="grid grid-cols-12 gap-x-8 gap-y-8">
+    <section className={cn('w-full py-16 overflow-x-hidden', bgColorClasses[backgroundColor])}>
+      <div className="container px-4 md:px-6">
+        <div className="grid grid-cols-12 gap-4 md:gap-8">
           {columns.map((column, index) => {
             /**
-             * This is just a small typescript hack, as do enhance all block types by the "size" property 
+             * This is just a small typescript hack, as do enhance all block types by the "size" property
              * using the appendSizeFieldToBlock function
              */
             const size = (column as any).size || 'half'
             return (
-              <div
-                key={index}
-                className={cn(
-                  'col-span-12',
-                  colsSpanClasses[size],
-                )}
-              >
+              <div key={index} className={cn('col-span-12', colsSpanClasses[size])}>
                 <RenderBlocks blocks={[column]} />
               </div>
             )
