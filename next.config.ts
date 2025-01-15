ed89eae94266137228bc75246ae52817b2911fe3
Tilman Marquart <til.marquart@gmail.com>
2025-01-16 00:13:21 +0100

    fix: remove uncommented code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
-import localization from './src/localization.config'
 
 
 export const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
@@ -24,70 +23,6 @@ const nextConfig: NextConfig = {
     ],
   },
   reactStrictMode: true,
-  // redirects: async () => {
-  //   return [
-  //     {
-  //       destination: '/ie-incompatible.html',
-  //       has: [
-  //         {
-  //           type: 'header',
-  //           key: 'user-agent',
-  //           value: '(.*Trident.*)', // all ie browsers
-  //         },
-  //       ],
-  //       permanent: false,
-  //       source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
-  //     },
-  //     // {
-  //     //   source: '/:locale/home',
-  //     //   destination: `/:locale`,
-  //     //   permanent: true,
-  //     // },
-  //   ]
-  // },
-  // rewrites: async () => {
-  //   const { locales, defaultLocale } = localization;
-  //   const nonDefaultLocales = locales.filter(locale => locale !== defaultLocale);
-  //   const protectedPath = [...nonDefaultLocales, 'home', 'api', 'admin', '_next'];
-  //   if (nonDefaultLocales.length > 0) {
-  //     return [
-  //       // /*  
-  //       // / --> /en/home 
-  //       // */
-  //       // {
-  //       //   source: '/',
-  //       //   destination: `/${defaultLocale}/home`,
-  //       // },
-  //       // /*
-  //       // /(de|es) --> /(de|es)/home
-  //       // */
-  //       // {
-  //       //   source: `/:locale(${nonDefaultLocales.join('|')})`,
-  //       //   destination: `/:locale/home`,
-  //       // },
-
-
-  //       /*
-  //       /:xyz(!de|es) --> /en/:xyz
-  //       */
-  //       {
-  //         source: `/:path((?!${protectedPath.join('|')}).*)`,
-  //         destination: `/${defaultLocale}/:path`,
-  //       },
-  //     ]
-  //   } else {
-  //     return [
-  //       {
-  //         source: '/',
-  //         destination: `/${defaultLocale}/home`,
-  //       },
-  //       {
-  //         source: `/:path.*`,
-  //         destination: `/${defaultLocale}/:path`,
-  //       },
-  //     ]
-  //   }
-  // },
   poweredByHeader: false,
 
 }
