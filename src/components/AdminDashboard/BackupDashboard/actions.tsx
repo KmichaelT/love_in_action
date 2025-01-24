6440ca69f47019b4b0a964ac37261cf78e193762
Tilman Marquart <til.marquart@gmail.com>
2025-01-24 17:55:08 +0100

    fix: url3

--- src/components/AdminDashboard/BackupDashboard/actions.tsx
+++ src/components/AdminDashboard/BackupDashboard/actions.tsx
@@ -22,12 +22,20 @@ export async function getDb() {
 }
 
 export function getCurrentDbName() {
-  const { hostname, pathname } = new URL(process.env.MONGODB_URI!);
-  return hostname + pathname;
+  try {
+    const { hostname, pathname } = new URL(process.env.MONGODB_URI!);
+    return hostname + pathname;
+  } catch (e) {
+    return "none";
+  }
 }
 
 export function getCurrentHostname() {
-  return process.env.NEXT_PUBLIC_SERVER_URL ? new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname : process.env.VERCEL_URL!;
+  try {
+    return process.env.NEXT_PUBLIC_SERVER_URL ? new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname : process.env.VERCEL_URL!;
+  } catch (e) {
+    return process.env.VERCEL_URL! || "none";
+  }
 }
 
 export async function restoreBackup(downloadUrl: string) {
